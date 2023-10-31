6d1f1aaeedab20c8182fcdb2dc21e61ea95cdc33
DeadScreams <alexsator.chanel@gmail.com>
2023-10-31 17:04:10 +0300

    ccwc challenge

---
+++ ccwc/ccwc.py
@@ -0,0 +1,55 @@
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='CCWC',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('filename', nargs='?', help='filename')
+parser.add_argument('-c', '--count', action='store_true', help='count bytes in a file')
+parser.add_argument('-l', '--lines', action='store_true', help='count lines in a file')
+parser.add_argument('-w', '--words', action='store_true', help='count words in a file')
+parser.add_argument('-m', '--characters', action='store_true', help='count characters in a file')
+args = parser.parse_args()
+
+if args.filename:
+    try:
+        with open(args.filename, 'r') as f:
+            data = f.read()
+    except FileNotFoundError:
+        print('File not found')
+        exit(1)
+else:
+    data = sys.stdin.read()
+
+class Counter:
+    def __init__(self, data):
+        self.data = data
+
+    def count_bytes(self):
+        return len(self.data)
+
+    def count_lines(self):
+        return len(self.data.split('\n'))
+
+    def count_words(self):
+        return len(self.data.split(' '))
+
+    def count_characters(self):
+        return len(self.data.replace(' ', ''))
+
+    def run(self):
+        if args.count:
+            print(self.count_bytes(), end=' ')
+        elif args.lines:
+            print(self.count_lines(), end=' ')
+        elif args.words:
+            print(self.count_words(), end=' ')
+        elif args.characters:
+            print(self.count_characters(), end=' ')
+        else:
+            print(f"Bytes: {self.count_bytes()}, Lines: {self.count_lines()}, Words: {self.count_words()}, Characters: {self.count_characters()}", end=' ')
+        if args.filename:
+            print(args.filename, end=' ')
+        print()
+
+Counter(data).run()
